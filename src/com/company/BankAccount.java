850fa1d87b2e2319626b7bcafb147c03e342ff10
SimonBison <simonpeterbison@gmail.com>
2017-04-24 15:04:34 +0200

    building up the showMenu method

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -53,6 +53,38 @@ class BankAccount
 
     void showMenu()
     {
+        char option='\0';
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome" +customerName);
+        System.out.println("Your ID is "+customerId);
+        System.out.println("\n");
+        System.out.println("A. Check Balance");
+        System.out.println("B. Deposit");
+        System.out.println("C. Withdraw");
+        System.out.println("D. Previous Transaction");
+        System.out.println("E. Exit");
+
+        do
+        {
+            System.out.println("=============================================================");
+            System.out.println("Enter an option");
+            System.out.println("=============================================================");
+            option = scanner.next().charAt(0);
+            System.out.println("\n");
+
+            switch(option)
+            {
+
+                case 'A':
+                    System.out.println("-----------------------------------------------------");
+                    System.out.println("Balance = " +balance);
+                    System.out.println("-----------------------------------------------------");
+                    System.out.println("\n");
+                    break;
+
+            }
+        }
 
 
     }
