ac5ef6329cae836d7ccbad84358b820ca9aef7de
SimonBison <simonpeterbison@gmail.com>
2017-04-24 14:32:58 +0200

    added transaction methods in the BankAccount class

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -5,5 +5,48 @@ package com.company;
  */
 class BankAccount
 {
+    int balance;               // current money in the account
+    int previousTransaction;   // last amount which is withdrawn or deposited
+    String customerName;
+    String customerId;
+
+    void deposit(int amount)
+    {
+        if(amount != 0)
+        {
+            balance = balance + amount;
+            previousTransaction = amount;
+        }
+    }
+
+    void withdraw(int amount)
+    {
+        if(amount != 0)
+        {
+            balance = balance - amount;
+            previousTransaction = -amount;
+        }
+    }
+
+    void getPreviousTransaction()
+    {
+        if(previousTransaction > 0)
+        {
+            System.out.println("Deposited:"+previousTransaction);
+        }
+        else if(previousTransaction < 0)
+        {
+            System.out.println("Withdrawn: "+Math.abs(previousTransaction));
+        }
+        else
+        {
+            System.out.println("No transaction occured");
+        }
+    }
+
+    void showMenu()
+    {
+
+    }
 
 }
